b61589873503cb06d28551895ecb2cdf7fa19314
Nishchit Dhanani <dnishchit@gmail.com>
2023-03-02 18:54:35 +0530

    launch: firecamp documentation getting started

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Firecamp</span>,
+  project: {
+    link: 'https://github.com/firecamp-io/firecamp',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/firecamp-io/firecamp',
+  footer: {
+    text: 'Firecamp Inc',
+  },
+}
+
+export default config
