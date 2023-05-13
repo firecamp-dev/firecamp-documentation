60d0d970565d225e8fbd8939662c95d47bd50348
Nishchit <dnishchit@gmail.com>
2023-05-13 19:30:50 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  head: (
-    <head>
-        <base href="/doc" target="_self"/>
-    </head>
-  ),
   logo: <span>Firecamp</span>,
   project: {
     link: 'https://github.com/firecamp-io/firecamp',
